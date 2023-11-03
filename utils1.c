dfbaf244b15ca37c250e738c2d18d85fa1e54888
LegendaryTunzeverywhere <princecph4@gmail.com>
2023-11-02 22:01:15 -0700

    Added more functions to utils1.c

--- utils1.c
+++ utils1.c
@@ -15,3 +15,69 @@ int de_lim(char c, char *dlim)
 			return (1);
 	return (0);
 }
+
+/**
+ * is_chain - Entry point
+ * @info: the struct list
+ * @buff: the char's buffer
+ * @a: address of current entity in buffer.
+ *
+ * Description: check if current char in the buffer is a chain delimeter
+ * Return: 1 on Success, 0 if failed.
+ */
+int is_chain(infos_t *info, char *buff, size_t *a)
+{
+	size_t i = *a;
+
+	if (buff[i] == '|' && buff[i + 1] == '|')
+	{
+		buff[i] = 0;
+		++i;
+		info->cmd_buff_type = CMD_OR;
+	}
+	else if (buff[i] == '&' && buff[i + 1] == '&')
+	{
+		buff[i] = 0;
+		info->cmd_buff_type = CMD_CHAIN;
+	}
+	else
+		return (0);
+	*a = i;
+	return (1);
+}
+
+/**
+ * if_chain - Entry point
+ * @info: struct lisy
+ * @buff: the cha's buffer
+ * @a: address of current position of entity in the buffer.
+ * @i: initial position in the buffer.
+ * @len: length of the buffer.
+ *
+ * Description: checks if chaining should continue based on is_chain.
+ *
+ * Return: nothing.
+ */
+void if_chain(infos_t *info, char *buff, size_t *a, size_t i, size_t len)
+{
+	size_t b = *a;
+
+	if (info->cmd_buff_type == CMD_AND)
+	{
+		if (info->stats)
+		{
+			buff[i] = 0;
+			b = len;
+		}
+		if (info->cmd_buff_type == CMD_OR)
+		{
+			if (!info->stats)
+			{
+				buff[i] = 0;
+				b = len;
+			}
+		}
+	}
+	*a = b;
+}
+
