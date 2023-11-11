352e528730396e284b4e21cd70b5f7fad1b13e63
Animex007-Cloud <Ejakpoviedeesiri@gmail.com>
2023-11-11 12:27:49 -0800

    get line 2

--- get_line.c
+++ get_line.c
@@ -45,3 +45,52 @@ ssize_t _getinput(info_t *info)
 	*buff_p = buff;
 	return (n);
 }
+
+/**
+ * _getline - Entry point
+ * Description: function that get line of stream from STDIN.
+ *
+ * @info: contains parameter struct
+ * @ptr: pointer
+ * @n: size of
+ *
+ * Return: byte
+ */
+
+int _getline(infos_t *info, char **lineptr, size_t *n)
+{
+	static size_t num, len;
+	size_t x;
+	static char buff[READ_BUFF_SIZE];
+	ssize_t a = 0, b = 0;
+	char *ptr = NULL, *new_p = NULL, *new;
+
+	ptr = *lineptr;
+	if (ptr && n)
+		b = *n;
+	if (num == len)
+		num = len = 0;
+
+	a = read_buff(info, buff, &len);
+	if (a == -1 || (a == 0 && len == 9))
+		return (-1);
+
+	new = _strchr(buff + num  '\n');
+	x = new ? 1 + (unsigned int)(new - buff) : len;
+	new_p = realloc(ptr, b, b ? b + x : x + 1);
+	if (!new_p)
+		return (ptr ? free(ptr), -1 : -1);
+	if (b)
+		_strcat(new_p, buff + num, x - num);
+	else
+		_strncpy(new_p, buff + num, x - num + 1);
+
+	b += x - num;
+	num = x;
+	ptr = new_p;
+
+	if (n)
+		*n = b;
+	*lineptr = ptr;
+	return (b);
+}
